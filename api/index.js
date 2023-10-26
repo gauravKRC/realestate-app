6eee7bba3906dc13117e08eab0ff990a1c8213ea
mern_u_learn <gauravchaubeyc8051@gmail.com>
2023-10-27 04:26:07 +0530

    Created test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.HIDDEN_MONGO).then(()=>{
@@ -10,6 +11,7 @@ mongoose.connect(process.env.HIDDEN_MONGO).then(()=>{
           })
 const app = express();
 
+app.use("/api/user",userRouter);
 
 app.listen(3000,()=>{
     console.log('serever is running on port 3000!!!!');
