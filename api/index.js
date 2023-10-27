7534117719123b8bc3579c385c64030b910408cd
mern_u_learn <gauravchaubeyc8051@gmail.com>
2023-10-27 19:37:43 +0530

    Created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.HIDDEN_MONGO).then(()=>{
@@ -11,8 +12,9 @@ mongoose.connect(process.env.HIDDEN_MONGO).then(()=>{
           })
 const app = express();
 
-app.use("/api/user",userRouter);
-
+app.use(express.json());
 app.listen(3000,()=>{
-    console.log('serever is running on port 3000!!!!');
+  console.log('serever is running on port 3000!!!!');
 })
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
